9e0a1d85098ad2cdf07212477509d5b0d6960b1c
Kehmiaf <105327432+Kehmiaf@users.noreply.github.com>
2022-05-23 12:21:45 -0400

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -7,3 +7,8 @@ def newMaven()
 {
   sh 'mvn package'
 }
+
+def newDeploy(ip,contextpath)
+{
+   deploy adapters: [tomcat9(credentialsId: 'f91a7a22-cfed-42ee-ac0a-0399de4446be', path: '', url: "${ip}")], contextPath: "${contextpath}", war: '**/*.war'
+}
